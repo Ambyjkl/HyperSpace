3f6dda9433438c7450f417f57036eeeca3eb32ec
Yash Mathur <yash97mathur@gmail.com>
2016-11-21 02:01:04 -0500

    working collision system

--- game.cpp
+++ game.cpp
@@ -8,8 +8,6 @@
 #include <stdlib.h>
 #include <iostream>
 
-// This is just an example using basic glut functionality.
-// If you want specific Apple functionality, look up AGL
 using namespace std;
 
 /*
@@ -20,11 +18,12 @@ using namespace std;
     4 -> 2 dense horizontal groups from either side of the screen
 */
 
-vector<pair<GLfloat, GLfloat> > bullets, aliens1, alens2, aliens3, aliens4;
-map<GLfloat, GLfloat> m;
-GLfloat r = 0.15;
-GLfloat pxl = -0.15, pxr = 0.15;
-bool play = true;
+vector<pair<GLfloat, GLfloat> > bullets, aliens1, aliens2, aliens3, aliens4;
+pair<GLfloat, GLfloat> alienUpdate;
+map<GLfloat, pair<GLfloat, int> > m;
+GLfloat r = 0.15, pxl = -0.15, pxr = 0.15;
+bool play = true, firstHit = false;
+int level = 0, cf = 1;
 
 void checkRedundant(){
 
@@ -34,7 +33,7 @@ pair<GLfloat, GLfloat> inc(pair<GLfloat, GLfloat> p){
     return make_pair(p.first, p.second+=0.1);
 }
 pair<GLfloat, GLfloat> incAlien(pair<GLfloat, GLfloat> p){
-    return make_pair(p.first, p.second-=0.1);
+    return make_pair(p.first, p.second-=0.005);
 }
 
 void displayBullets(){
@@ -42,24 +41,103 @@ void displayBullets(){
         cout<<bullets[i].first<<", "<<bullets[i].second<<endl;
 }
 
-bool detectCollision(int formation){
-    //cout<<"Hello";
-    m.clear();
-    for(int i=0;i<aliens1.size();++i){
-        //cout<<i<<" "<<aliens1[i].first<<" "<<aliens1[i].second<<endl<<endl;
-        m[aliens1[i].second] = aliens1[i].first;
-    }
-    for(int i=0;i<bullets.size();++i)
-        if(m.find(bullets[i].second)!=m.end()){
-            cout<<m[bullets[i].second+r]<<": "<<bullets[i].first+r<<endl<<m[bullets[i].second+r]<<": "<<bullets[i].first-r<<endl<<endl;
-            if(m[bullets[i].second+r]<=bullets[i].first+r && m[bullets[i].second+r]>=bullets[i].first-r)
-                return true;
+void showAliens(int);
+bool aliensGone(int);
+void DrawStarFilled (float cx, float cy, float radius, int numPoints);
+void genAliens(int);
+
+void showAliens(int formation){
+    if(formation==1){
+        for(int i=0;i<aliens1.size();i++){
+            DrawStarFilled(aliens1[i].first, aliens1[i].second, 0.1, 5);
         }
+        transform(begin(aliens1), end(aliens1), begin(aliens1), incAlien);
+    }
+}
+
+
+bool aliensGone(int formation){
+    int a[4];
+    a[0] = aliens1.size();
+    a[1] = aliens2.size();
+    a[2] = aliens3.size();
+    a[3] = aliens4.size();
+    //cout<<a[0]<<endl;
+    if(a[formation-1]==0){
+        cout<<"it is 0\n";
+        return true;
+    }
     return false;
 }
 
-void DrawStarFilled (float cx, float cy, float radius, int numPoints){
+//temporary
+int newFormation(){
+    return 1;
+}
 
+bool detectCollision(int formation){
+    m.clear();
+    for(int i=0;i<aliens1.size();++i)
+        m[aliens1[i].second] = make_pair(aliens1[i].first, i);
+
+    /*for(int i=0;i<bullets.size();++i){
+        //cout<<"main loop\n";
+        if(m.find(bullets[i].second)!=m.end()){
+            cout<<"something\n";
+            if(m[bullets[i].second+r].first<=bullets[i].first+r &&
+               m[bullets[i].second+r].first>=bullets[i].first-r){
+                    aliens1.erase(aliens1.begin()+m[bullets[i].second].second);
+                    bullets.erase(bullets.begin()+i);
+                    cout<<"collision\n";
+                    return true;
+            }
+        }
+    }*/
+    for(int i=0;i<bullets.size();++i){
+        for(int j=0;j<aliens1.size();++j){
+            if(fabs((float)(aliens1[j].second-bullets[i].second))<=0.1){
+                //cout<<"something\n";
+                if(fabs((float)(aliens1[j].first-bullets[i].first))<=r){
+                    aliens1.erase(aliens1.begin()+j);
+                    bullets.erase(bullets.begin()+i);
+                    firstHit = true;
+                    //cout<<"collision "<<aliens1.size()<<"\n";
+                    return true;
+                }
+            }
+        }
+    }
+    if(firstHit)
+        if(aliensGone(cf)){
+            cout<<"gone\n";
+            cf = 1;
+            genAliens(cf);
+            firstHit = false;
+            //cout<<cf<<endl;
+        }
+    //if(firstHit)
+    //    cout<<"something is wrong\n";
+    showAliens(cf);
+    if(bullets.size()>=0 && play)
+        transform(begin(bullets), end(bullets), begin(bullets), inc);
+
+    return false;
+}
+
+void genAliens(int formation){
+    aliens1.clear();
+    aliens2.clear();
+    aliens3.clear();
+    aliens4.clear();
+    if(formation==1){
+        for(GLfloat i=-1;i<=1;i+=0.4){
+            //cout<<i<<endl;
+            aliens1.push_back(make_pair(i, 2));
+        }
+    }
+}
+
+void DrawStarFilled (float cx, float cy, float radius, int numPoints){
     const float DegToRad = 3.14159 / 180;
 
     glBegin (GL_TRIANGLE_FAN);
@@ -125,22 +203,26 @@ void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    DrawStarFilled(0, 2, 0.1, 5);
+    //DrawStarFilled(0, 2, 0.1, 5);
 
-    if(play)
-        if(detectCollision(1))
-            play = false;
 
-    //Player
+
+    //Drawing the player
     glBegin(GL_TRIANGLES);
         glVertex3f(pxl, -2.6, -10.0);
         glVertex3f(float((pxl+pxr)/2), -2.4, -10.0);
         glVertex3f(pxr, -2.6, -10.0);
     glEnd();
 
+    //generating harambaes <3 <3
+
+
+    if(play){
+        if(detectCollision(1))
+            play = true;
+    }
+
     //Moving the bullets every frame
-    if(bullets.size()>=0 && play)
-        transform(begin(bullets), end(bullets), begin(bullets), inc);
 
     for(int i=0;i<bullets.size();++i){
         //DrawCircle(bullets[i].first, bullets[i].second, 0.1, 0.2);
@@ -150,7 +232,6 @@ void display(){
             glVertex3f(bullets[i].first+r, bullets[i].second-r, -10.0);
         glEnd();
     }
-
     glutSwapBuffers();
 }
 
@@ -167,20 +248,18 @@ void reshape(int w, int h){
 
 int main(int argc, char **argv){
     glutInit(&argc, argv); // Initializes glut
-
     // Sets up a double buffer with RGBA components and a depth component
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
-
     // Sets the window size to 512*512 square pixels
     glutInitWindowSize(512, 1024);
-
     // Sets the window position to the upper left
     glutInitWindowPosition(0, 0);
-
     // Creates a window using internal glut functionality
     glutCreateWindow("HyperSpace");
 
-    aliens1.push_back(make_pair(0, 2));
+
+    //cf = newFormation();
+    genAliens(cf);
     // passes reshape and display functions to the OpenGL machine for callback
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
@@ -196,14 +275,14 @@ int main(int argc, char **argv){
 //Redundant for now
 //Do not touch this part
 
-GLuint LoadTexture( const char * filename ){
+GLuint LoadTexture(const char *filename){
     GLuint texture;
     int width, height;
-    unsigned char * data;
-    FILE * file;
-    file = fopen( filename, "rb" );
+    unsigned char *data;
+    FILE *file;
+    file = fopen(filename, "rb");
 
-    if ( file == NULL )
+    if(file==NULL)
         return 0;
     width = 1024;
     height = 512;
@@ -220,7 +299,6 @@ GLuint LoadTexture( const char * filename ){
 
         data[index] = R;
         data[index+2] = B;
-
     }
 
     glGenTextures( 1, &texture );
@@ -232,7 +310,7 @@ GLuint LoadTexture( const char * filename ){
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
     gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,GL_RGB, GL_UNSIGNED_BYTE, data );
-    free( data );
+    free(data);
 
     return texture;
 }
